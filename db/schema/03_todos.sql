ce66a01914f72376d8693460c41bb0ea894264f6
JordanBandur <jordanbandur@hotmail.ca>
2024-05-03 14:51:22 -0700

    Add table for todos

---
+++ db/schema/03_todos.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS todos CASCADE;
+CREATE TABLE todos (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    category_id INTEGER NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE, -- Automatically deletes all todos associated with a user when that user is deleted.
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
+);
