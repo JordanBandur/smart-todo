2f901c3b8c1ff4a088e02b2ae7dcc24b1003ac4a
JordanBandur <jordanbandur@hotmail.ca>
2024-05-03 14:49:53 -0700

    Update seed for users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,22 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name) VALUES
+('Laura'),
+('Max'),
+('Sophia'),
+('Ethan'),
+('Olivia'),
+('Zachary'),
+('Amelia'),
+('Liam'),
+('Emma'),
+('Noah'),
+('Mia'),
+('Lucas'),
+('Isabella'),
+('Mason'),
+('Ava'),
+('Logan'),
+('Lily'),
+('James'),
+('Emily'),
+('Alexander');
