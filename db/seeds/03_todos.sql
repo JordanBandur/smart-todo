c03eaa1e2c5132de8444c1c6ab890b18e8b5c3cc
JordanBandur <jordanbandur@hotmail.ca>
2024-05-03 14:50:48 -0700

    Add seeds for todos

---
+++ db/seeds/03_todos.sql
@@ -0,0 +1,101 @@
+INSERT INTO todos (user_id, title, category_id) VALUES
+
+(1, 'Watch The Crown', 1),
+(1, 'Eat at Chez Panisse', 2),
+(1, 'Read Little Women', 3),
+(1, 'Buy a yoga mat', 4),
+
+(2, 'Watch Interstellar', 1),
+(2, 'Eat at Bouchon', 2),
+(2, 'Read Sapiens', 3),
+(2, 'Buy a mountain bike', 4),
+
+(3, 'Watch Friends', 1),
+(3, 'Eat at Nobu', 2),
+(3, 'Read Pride and Prejudice', 3),
+(3, 'Buy a new perfume', 4),
+
+(4, 'Watch Narcos', 1),
+(4, 'Eat at Smorgasburg', 2),
+(4, 'Read The Hobbit', 3),
+(4, 'Buy a camera', 4),
+
+(5, 'Watch Mad Men', 1),
+(5, 'Eat at The Slanted Door', 2),
+(5, 'Read 934', 3),
+(5, 'Buy a Kindle', 4),
+
+(11, 'Watch The Office', 1),
+(11, 'Eat at Franklin Barbecue', 2),
+(11, 'Read The Great Gatsby', 3),
+(11, 'Buy a new laptop', 4),
+
+(7, 'Watch Downton Abbey', 1),
+(7, 'Eat at Canlis', 2),
+(7, 'Read Wuthering Heights', 3),
+(7, 'Buy an electric kettle', 4),
+
+(8, 'Watch Lost', 1),
+(8, 'Eat at the KEG', 2),
+(8, 'Read Fahrenheit 451', 3),
+(8, 'Buy a new backpack', 4),
+
+(9, 'Watch Sherlock', 1),
+(9, 'Eat at Alinea', 2),
+(9, 'Read Crime and Punishment', 3),
+(9, 'Buy a new phone', 4),
+
+(10, 'Watch Stranger Things', 1),
+(10, 'Eat at Katz''s Delicatessen', 2),
+(10, 'Read Brave New World', 3),
+(10, 'Buy a new TV', 4),
+
+(11, 'Watch The Mandalorian', 1),
+(11, 'Eat at Joe''s Stone Crab', 2),
+(11, 'Read Anna Karenina', 3),
+(11, 'Buy a new couch', 4),
+
+(12, 'Watch Breaking Bad', 1),
+(12, 'Eat at Din Tai Fung', 2),
+(12, 'Read To Kill a Mockingbird', 3),
+(12, 'Buy a new bike', 4),
+
+(13, 'Watch Game of Thrones', 1),
+(13, 'Eat at El Celler de Can Roca', 2),
+(13, 'Read The Catcher in the Rye', 3),
+(13, 'Buy a new dress', 4),
+
+(14, 'Watch The Wire', 1),
+(14, 'Eat at Franklin Barbecue', 2),
+(14, 'Read Les Misérables', 3),
+(14, 'Buy a guitar', 4),
+
+(15, 'Watch The Sopranos', 1),
+(15, 'Eat at Blue Hill', 2),
+(15, 'Read Don Quixote', 3),
+(15, 'Buy a new tablet', 4),
+
+(16, 'Watch Fargo', 1),
+(16, 'Eat at The French Laundry', 2),
+(16, 'Read 934', 3),
+(16, 'Buy running shoes', 4),
+
+(17, 'Watch Peaky Blinders', 1),
+(17, 'Eat at Gordon Ramsay''s Hell''s Kitchen', 2),
+(17, 'Read Jane Eyre', 3),
+(17, 'Buy a handbag', 4),
+
+(18, 'Watch Twin Peaks', 1),
+(18, 'Eat at Pizzeria Bianco', 2),
+(18, 'Read Atlas Shrugged', 3),
+(18, 'Buy a new watch', 4),
+
+(19, 'Watch Black Mirror', 1),
+(19, 'Eat at Eleven Madison Park', 2),
+(19, 'Read The Picture of Dorian Gray', 3),
+(19, 'Buy a set of headphones', 4),
+
+(20, 'Watch The Queen''s Gambit', 1),
+(20, 'Eat at Joe''s Crab Shack', 2),
+(20, 'Read Catch-22', 3),
+(20, 'Buy a new PC', 4);
