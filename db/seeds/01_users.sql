c56b24422b76e7e546764dc88b158753b54d8629
JordanBandur <jordanbandur@hotmail.ca>
2024-05-06 11:43:23 -0700

    Add default passwords for users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, password) VALUES
-('ByteMe', 'pass'),
-('CacheCow', 'pass'),
-('BinaryBard', 'pass'),
-('ArrayOfSunshine', 'pass');
+INSERT INTO users (username) VALUES
+('ByteMe'),
+('CacheCow'),
+('BinaryBard'),
+('ArrayOfSunshine');
