639ce7f87b84d17febd774258f35c09a67fdccab
JordanBandur <jordanbandur@hotmail.ca>
2024-05-05 20:25:36 -0700

    Comment out code that was causing error.

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
-app.use('/todos', addTaskRoutes);
+//app.use('/todos', addTaskRoutes);
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
