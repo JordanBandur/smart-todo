34bc25f4baf22deec1fb16d38cc31c81a5afe161
JordanBandur <jordanbandur@hotmail.ca>
2024-05-07 08:01:55 -0700

    Un-comment DB-params

--- db/connection.js
+++ db/connection.js
@@ -3,13 +3,13 @@ const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 const saltRounds = 10;
 
-// const dbParams = {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME
-// };
+const dbParams = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+};
 
 
 const db = new Pool(dbParams);
