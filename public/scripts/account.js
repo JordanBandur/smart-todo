b167d3605c258f6085cc1760ee170fb3ac0c61f2
JordanBandur <jordanbandur@hotmail.ca>
2024-05-06 22:13:05 -0700

    Add logic for interacting with account.ejs

---
+++ public/scripts/account.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  // Open and close modal logic
+  $('#accountBtn').click(function() {
+    $('#accountModal').show().attr('aria-hidden', 'false');
+  });
+
+  $('.modal .close').click(function() {
+    $('#accountModal').hide().attr('aria-hidden', 'true');
+  });
+
+  $(window).click(function(event) {
+    if ($(event.target).is('.modal')) {
+      $('#accountModal').hide().attr('aria-hidden', 'true');
+    }
+  });
+
+  // Function to set action and submit form
+  const setAction = function(action) {
+    $('#action').val(action); // Set the action type
+    console.log($('#username').val()); // Log the username to ensure it's captured correctly
+    $('form').submit(); // Submit the form
+  };
+
+  $('.update-button').click(function() {
+    setAction('update');
+  });
+
+  $('.logout-button').click(function() {
+    setAction('logout');
+  });
+});
