c34a1ce3e6ec7a1f62bd4735009a7fe7cd503338
JordanBandur <jordanbandur@hotmail.ca>
2024-05-07 10:59:11 -0700

    Add user session

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: 'your_secret_key', // Make sure to keep it confidential
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
