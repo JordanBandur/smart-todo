31359e2fbe314bd93d4a5b6fb64ef2b39a7afe63
JordanBandur <jordanbandur@hotmail.ca>
2024-05-08 11:30:54 -0700

    Remove hasing method

--- db/connection.js
+++ db/connection.js
@@ -11,42 +11,7 @@ const dbParams = {
   database: process.env.DB_NAME
 };
 
-
 const db = new Pool(dbParams);
 
-// Will hash the passwords in the database
-const hashAndUpdatePasswords = async function() {
-  const fetchQuery = 'SELECT id, password FROM users';
-  const updateQuery = 'UPDATE users SET password = $1 WHERE id = $2';
-
-  try {
-    const users = await db.query(fetchQuery);
-
-    for (let user of users.rows) {
-      // Ensure the password isn't already hashed (in case this script is run multiple times)
-      if (user.password.startsWith('$2b$')) {
-        console.log('Skipping already hashed password for user id:', user.id);
-        continue;
-      }
-      const hashedPassword = await bcrypt.hash(user.password, saltRounds);
-      await db.query(updateQuery, [hashedPassword, user.id]);
-    }
-
-    console.log('All passwords have been updated successfully.');
-  } catch (err) {
-    console.error('Error during password hashing and updating:', err);
-  }
-};
-
-hashAndUpdatePasswords();
-
-// // tests connection
-// db.query('SELECT NOW()', (err, res) => {
-//   if (err) {
-//     console.error('Connection error', err.stack);
-//   } else {
-//     console.log('Successful database connection', res.rows[0].now); // Log the current time from DB
-//   }
-// });
 
 module.exports = db;
