32ad78f8783c8a244af9638176a2f476522915b6
JordanBandur <jordanbandur@hotmail.ca>
2024-05-08 11:59:38 -0700

    Add column for completed todo

--- db/schema/03_todos.sql
+++ db/schema/03_todos.sql
@@ -6,6 +6,7 @@ CREATE TABLE todos (
     category_id INTEGER NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    completed BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE, -- Automatically deletes all todos associated with a user when that user is deleted.
     FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
