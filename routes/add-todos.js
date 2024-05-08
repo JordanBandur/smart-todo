83b39c0e434a72fcae0039076d27bba5746331c9
JordanBandur <jordanbandur@hotmail.ca>
2024-05-07 23:31:15 -0700

    Add GET route for todos

--- routes/add-todos.js
+++ routes/add-todos.js
@@ -4,17 +4,46 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const categorizeTask = require('../helpers/categorizeTask');
 const { addToDo } = require('../helpers/database');
+const db = require('../db/connection');
+const { fetchAndCategorizeTodos } = require('../helpers/fetchAndCategorizeTodos');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-// hardcoded default user ID for testing
+// Gets the user's todos from the DB and displays the html in todos.ejs
+
+router.get('/', async (req, res) => {
+  if (!req.session || !req.session.user) {
+    return res.status(401).json({ error: 'Unauthorized' });
+}
+  try {
+      const todosResult = await db.query(`
+          SELECT todos.title, categories.name AS category_name
+          FROM todos
+          JOIN categories ON todos.category_id = categories.id
+          ORDER BY categories.name, todos.created_at DESC;
+      `);
+      const todos = todosResult.rows;
+
+      const categorizedTodos = todos.reduce((acc, todo) => {
+          if (!acc[todo.category_name]) {
+              acc[todo.category_name] = [];
+          }
+          acc[todo.category_name].push(todo.title);
+          return acc;
+      }, {});
+
+      res.json({categorizedTodos }); // Send this data as JSON
+  } catch (err) {
+      console.error('Error fetching todos:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 router.post('/', async (req, res) => {
   try {
     const taskDescription = req.body['new-todo'];
-
 
     // Call the categorizeTask function to categorize the task
     let category = await categorizeTask(taskDescription);
@@ -27,7 +56,7 @@ router.post('/', async (req, res) => {
       category = 'Film/Series';
     }
 
-    console.log(category)
+    console.log(category);
 
     // Determine the list ID based on the category
     const listIds = {
