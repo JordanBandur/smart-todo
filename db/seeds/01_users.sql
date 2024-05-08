8eff715dcd1145f9bcaced68b5b4f34087012e08
JordanBandur <jordanbandur@hotmail.ca>
2024-05-08 11:31:17 -0700

    Update seeded users to have hashed passwords

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO users (username, password) VALUES
-('ByteMe', 'password'),
-('CacheCow', 'password'),
-('BinaryBard', 'password'),
-('ArrayOfSunshine', 'password');
+('ByteMe', '$2b$10$3qACrB85CHqbNekzbzOucuNP/16pV8aGg0eVpEtFfRbjBLEouKUc.'),
+('CacheCow', '$2b$10$3qACrB85CHqbNekzbzOucuNP/16pV8aGg0eVpEtFfRbjBLEouKUc.'),
+('BinaryBard', '$2b$10$3qACrB85CHqbNekzbzOucuNP/16pV8aGg0eVpEtFfRbjBLEouKUc.'),
+('ArrayOfSunshine', '$2b$10$3qACrB85CHqbNekzbzOucuNP/16pV8aGg0eVpEtFfRbjBLEouKUc.');
