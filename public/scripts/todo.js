7477010d3bbc344e862a8dcae9d4651f22b0f0c2
JordanBandur <jordanbandur@hotmail.ca>
2024-05-08 13:51:28 -0700

    Alter post logic to prepend the new todo

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -50,10 +50,16 @@ $(document).ready(function() {
       url: '/todos/',
       data: { 'new-todo': taskDescription },
       success: function(response) {
-        const { category, listId } = response;
-        const listItem = $('<li>').text(taskDescription);
-        $('#' + listId).append(listItem);
-        fetchTodos();
+
+        const { category } = response;
+        const categoryName = (typeof category === 'object' && category.name) ? category.name : category;
+        if (typeof categoryName === 'string') {
+          const listId = categoryName.toLowerCase().replace(/\s+/g, '-') + '-list';
+          const listItem = $('<li>').text(taskDescription);
+        $('#' + listId).prepend(listItem);
+        }
+
+        $('#new-todo').val('');
       },
       error: function(xhr, status, error) {
         console.error('Error adding task:', error);
